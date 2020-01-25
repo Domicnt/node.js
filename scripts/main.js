a205090d3df02b36d50f377a137d19a5477c94d9
Dominic-b <42015317+Dominic-b@users.noreply.github.com>
2020-01-24 20:48:42 -0800

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,69 @@
+let canvas = document.getElementById('pong');
+let context = document.getElementById('pong').getContext('2d');
+
+let width = window.innerWidth - 2;
+let height = window.innerHeight - 4;
+
+context.canvas.width = width;
+context.canvas.height = height;
+
+let ID = 0;
+
+let ws;
+
+buildwebsocket();
+function buildwebsocket() {
+    ws = new WebSocket("ws://127.0.0.1:80", 'echo-protocol');
+    ws.onopen = (evt) => { console.log('Connected'); };
+    ws.onclose = (evt) => { console.log('Connection closed'); };
+    ws.onmessage = (evt) => {
+        if (evt.data.slice(0, 3) == 'ID:') {
+            ID = evt.data.split(':', 2)[1];
+        }
+        else {
+            context.clearRect(0, 0, width, height);
+            let playersData = evt.data.split('|');
+            for (let i = 0; i < playersData.length; i++) {
+                let playerData = playersData[i].split(':');
+                context.beginPath();
+                context.arc(playerData[0], playerData[1], playerData[2], 0, 2 * Math.PI);
+                context.stroke();
+            }
+        }
+
+        console.log('Message: ' + evt.data);
+    };
+    ws.onerror = (evt) => { console.log('Connection error'); };
+}
+
+function send(message) {
+    console.log("SENT: " + message); 
+    ws.send(ID + ':' + message);
+}
+
+var keyArr = [];
+document.onkeydown = (event) => {
+    event = event || window.event;
+    let charCode = event.keyCode || event.which;
+    keyArr[charCode] = true;
+}
+document.onkeyup = (event) => {
+    event = event || window.event;
+    let charCode = event.keyCode || event.which;
+    keyArr[charCode] = false;
+}
+
+
+function sendKeys() {
+    for (let i = 0; i < keyArr.length; i++) {
+        if (keyArr[i] === true) {
+            send(String.fromCharCode(i));
+        }
+    }
+}
+
+setInterval(sendKeys, 50);
+
+function close() {
+    ws.close();
+}
---
+++ server.js
@@ -0,0 +1,100 @@
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+
+var WebSocketServer = require('websocket').server;
+
+let players = [[0,0,25,0]];
+
+let server = http.createServer((request, response) => {
+    if (request.url === "/") {
+        request.url = "/index.html";
+    }
+    fs.readFile(__dirname + request.url, (error, data) => {
+        let fileTypes =
+        {
+            html: "text/html",
+            css: "text/css",
+            js: "application/javascript"
+        };
+        let fileType = request.url.split(".").pop();
+        response.writeHead(200, { 'Content-Type': fileTypes[fileType] });
+        response.write(data);
+
+        response.end();
+    });
+});
+
+server.listen(80);
+
+
+
+//WebSocket
+wsServer = new WebSocketServer({
+    httpServer: server,
+    // You should not use autoAcceptConnections for production 
+    // applications, as it defeats all standard cross-origin protection 
+    // facilities built into the protocol and the browser.  You should 
+    // *always* verify the connection's origin and decide whether or not 
+    // to accept it. 
+    autoAcceptConnections: false
+});
+
+function originIsAllowed(origin) {
+    // put logic here to detect whether the specified origin is allowed. 
+    return true;
+}
+
+let connections = [];
+
+wsServer.on('request', (request) => {
+    if (!originIsAllowed(request.origin)) {
+        // Make sure we only accept requests from an allowed origin 
+        request.reject();
+        console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
+        return;
+    }
+    let connection = request.accept('echo-protocol', request.origin);
+
+    //store the new connection in your array of connections and send ID
+    connections.push(connection);
+    connections[connections.length - 1].sendUTF('ID:' + connections.length);
+
+    players.push([0,0,25, connections.length]);
+
+    console.log((new Date()) + ' Connection accepted.');
+    connection.on('message', (message) => {
+        if (message.type === 'utf8') {
+            console.log('Received Message: ' + message.utf8Data);
+            let ID = message.utf8Data.split(':', 1);
+            let str = message.utf8Data.split(':', 2)[1];
+            if (str === 'W') {
+                players[ID - 1][1] -= 5;
+            }
+            if (str === 'S') {
+                players[ID - 1][1] += 5;
+            }
+            if (str === 'A') {
+                players[ID - 1][0] -= 5;
+            }
+            if (str === 'D') {
+                players[ID - 1][0] += 5;
+            }
+        }
+        let str = '';
+        for (let i = 0; i < connections.length; i++) {
+            if (i > 0) str += '|';
+            for (let j = 0; j < 3; j++) {
+                if (j > 0) str += ':' + players[i][j];
+                else str += players[i][j];
+            }
+        }
+        for (let i = 0; i < connections.length; i++) {
+            connections[i].sendUTF(str);
+        }
+    });
+    
+    connection.on('close', (reasonCode, description) => {
+        console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+    });
+});
